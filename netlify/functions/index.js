ceb533ec033487e7aa1fa7f8cc46e755cb1ce5e0
wghiacy <wghiacy@gmail.com>
2025-06-23 13:27:47 -0600

    Update index.js

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -10,10 +10,9 @@ export default async (request) => {
     { headers }
   );
 
-  // Copy all relevant headers from the upstream response
+  // Copy all headers from the upstream response
   const responseHeaders = new Headers(videoResponse.headers);
   responseHeaders.set("content-type", "video/mp4");
-  // Optionally set CORS headers
   responseHeaders.set("Access-Control-Allow-Origin", "*");
 
   return new Response(videoResponse.body, {
